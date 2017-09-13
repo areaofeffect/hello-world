726acbfa30787c8ebe26774aac318cc8ce7326d6
Carrie Kengle <carriesteady@gmail.com>
2017-09-12 22:09:00 -0400

    in class example code

---
+++ week2/examples/python/data_types.py
@@ -0,0 +1,41 @@
+myBool = True
+print(myBool)
+
+myString = "Hello World"
+print(myString)
+
+word1 = "hello "
+word2 = "world"
+print (word1 + word2 + " " + myString)
+
+myInt = 1
+print(myInt)
+
+myFloat = 3.14
+print(myFloat)
+
+print(myInt + myFloat)
+
+myList = ["Water", "Apple", "Banana"]
+print myList
+print myList[2] #this will print Water
+
+myDict = {
+  "fuel_type": "electric", 
+  "model": "S", 
+  "color": "red"
+}
+print myDict
+print myDict["color"]
+
+
+myString = "1"
+myNum = int(myString)
+
+print myNum
+
+
+
+
+
+
---
+++ week2/examples/python/object_type.py
@@ -0,0 +1,9 @@
+imacObj = {
+  "screenSize": "HD",
+  "screenHeight": 1080,
+  "screenWidth": 1920,
+  "color": "Space Grey"
+} 
+
+print imacObj["screenHeight"]
+
