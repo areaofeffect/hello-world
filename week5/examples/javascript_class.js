f0682a2b4830b298c560afbcc409474ce5eb69df
Carrie Kengle <carriesteady@gmail.com>
2018-10-03 16:32:43 -0400

    Update javascript_class.js

--- week5/examples/javascript_class.js
+++ week5/examples/javascript_class.js
@@ -1,23 +1,28 @@
-var pokemonModule = function(inName) {
-  this.name = inName;
-  this.health = 100;
-  
+var pokemonModule = function(inName, inHealth) {
+  var name = inName;
+  var health = inHealth;
+
   function sayHello() {
-    console.log("hello i'm", this.name);
+    console.log("hello i'm", name);
   }
-  
-  function updateHealth(num) {
-    this.health += num;
+
+  function updateHealth(inNum) {
+    health += inNum;
+    console.log(health);
   }
-  
+
+  function getHealth() {
+    return health;
+  }
+
   return {
-    "sayHello": sayHello,
-    "updateHealth": updateHealth,
-    health: this.health,
-    name: this.name
+    sayHello: sayHello,
+    updateHealth: updateHealth,
+    getHealth: getHealth
   }
 }
-var pikachu = new pokemonModule('pikachu');
+var pikachu = new pokemonModule('pikachu', 100);
 pikachu.sayHello();
-pikachu.updateHealth(10);
-console.log(pikachu.health);
+pikachu.updateHealth(100);
+
+console.log(pikachu.getHealth());
