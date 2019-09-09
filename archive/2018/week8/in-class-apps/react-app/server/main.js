f4c30ad2b8f7bd8f7120125872f923f1bd074922
Carrie Kengle <carriesteady@gmail.com>
2019-09-09 11:29:53 -0400

    archive

---
+++ archive/2018/week8/in-class-apps/react-app/server/main.js
@@ -0,0 +1,44 @@
+import { Meteor } from 'meteor/meteor';
+import { HTTP } from 'meteor/http';
+import { Asteroids } from '../imports/api/asteroids.js'
+
+
+Meteor.publish('asteroids', function asteroidsPublication() {
+  return Asteroids.find();
+});
+
+Meteor.methods({
+  loadAsteroids: function() {
+    console.log("loadAsteroids");
+
+    HTTP.call('GET', 'https://api.nasa.gov/neo/rest/v1/feed', {
+      params: { 
+        api_key: "iyuaalFBShG6fxUdb0muEx616dsZEdkTIFqQ9iV0", 
+        start_date:"2018-09-17", 
+        end_date:"2018-09-24" 
+      }
+    }, (error, result) => {
+      console.log(error);
+      if (!error) {
+        var data = result.data;
+        console.log(data["near_earth_objects"])
+
+        for (var dateKey in data["near_earth_objects"]) {
+          for (var i = 0; i < data["near_earth_objects"][dateKey].length; i++) {
+            console.log(data["near_earth_objects"][dateKey][i].name);
+
+            Asteroids.insert({text:data["near_earth_objects"][dateKey][i].name, date:new Date()})
+
+          }
+        }
+
+      }
+    });
+
+  }
+})
+
+
+Meteor.startup(() => {
+  // code to run on server at startup
+});
