bac421fd700167f531c0ed8019b184d66795643b
Carrie Kengle <carrie@ckisadj.com>
2021-11-05 17:33:26 -0400

    week9, IoT

---
+++ week9/README.md
@@ -0,0 +1,26 @@
+# IoT
+
+# SVA IxD | Week 9
+
+
+# MQTT
+
+Let's install [MQTTx](https://mqttx.app/) and set up some client connections for sending and receiving messages
+
+
+## Testing MQTT
+Now that you have the MQTTx installed let's create some clients and test publishing and subscribing to messages. 
+
+## Client 1 (MQTT via TCP)
+
+## Client 2 (MQTT via Websockets)
+
+
+## Combining MQTT with P5js
+
+We will add MQTT to [a p5.js app]().
+
+
+
+
+
---
+++ week9/p5/sketch.js
@@ -0,0 +1,72 @@
+
+// MQTT client details:
+let broker = {
+  hostname: '23.21.151.236',
+  port: 9001,
+  protocol: 'mqtt'
+};
+
+// MQTT client:
+let client;
+
+
+// helper function to create unique identifier for client
+//https://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid
+function uuidv4() {
+  return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+    (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+  );
+}
+
+// client credentials:
+let creds = {
+  clientID: 'p5Client_'+uuidv4(),
+}
+
+// called when the client connects
+function onConnect() {
+  console.log("client connected");
+  // once connected will subscribe to our topic
+  // # is a wildcard that subscribes us to any topic that starts with sva/
+  client.subscribe("sva/#");
+}
+
+// called when the client loses its connection
+function onConnectionLost(response) {
+  console.log("client not connected");
+}
+
+// called when a message arrives
+function onMessageArrived(message) {
+  console.log("message arrived");
+  // this is the topic
+  console.log(message.destinationName);
+  // this is the message payload
+  console.log("message as string: ", message.payloadString);
+  // we can convert our string into a json obj
+  const obj = JSON.parse(message.payloadString);
+  console.log("message as JSON obj", obj)
+}
+
+
+function setup() {
+  createCanvas(400, 400);
+  console.log(creds)
+  // assign a value to our client variable
+  client = new Paho.MQTT.Client(broker.hostname, Number(broker.port), creds.clientID);
+  // set callback handlers for the client:
+  client.onConnectionLost = onConnectionLost;
+  client.onMessageArrived = onMessageArrived;
+  // connect to the MQTT broker:
+  client.connect(
+    {
+        onSuccess: onConnect,       // callback function for when you connect
+    }
+  );
+}
+
+function draw() {
+  background(220);
+}
+
+
